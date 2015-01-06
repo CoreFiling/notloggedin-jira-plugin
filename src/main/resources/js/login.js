1cd27889cc9f41e9e4cde9efd746ae7cbb3a1fa7
Paul Cadman <pwc@corefiling.com>
2015-01-06 11:04:29 +0000

    Use the global variable 'contextPath' for constructing the JIRA login URL.
    JIRA provides 'contextPath' in global scope for scripts to construct
    relative URLs.

--- src/main/resources/js/login.js
+++ src/main/resources/js/login.js
@@ -1,1 +1,2 @@
-window.location.href = "/jira/login.jsp?os_destination=" + window.location.href;
+// contextPath is a global variable provided by JIRA for constructing relative paths
+window.location.href = contextPath + "/login.jsp?os_destination=" + window.location.href;
