383cd663f5fabcbb00ae37517fd15f629f86f64f
Matthew Law <mjl@corefiling.com>
2017-05-08 13:56:40 +0100

    Update to be in line with the JIRA 7 plugin template/structure

--- src/main/resources/js/login.js
+++ src/main/resources/js/login.js
@@ -1,2 +1,2 @@
 // contextPath is a global variable provided by JIRA for constructing relative paths
-window.location.href = contextPath + "/login.jsp?os_destination=" + window.location.href;
+window.location.href = contextPath + "/login.jsp?os_destination=" + window.location.href
